a94cf213c35d666bbeba654cbc8ff2da24fdb549
trocky321 <tejasvi.qa@gmail.com>
2016-02-14 21:26:18 -0500

    Adding mior fixes

--- src/main/java/com/sample/core/TestBase.java
+++ src/main/java/com/sample/core/TestBase.java
@@ -4,8 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+
 public class TestBase extends Driver{
 	public static WebDriver driver;
+	
 	@BeforeClass
 	public void startUp(){
 		driver=Driver.initDriver("FIREFOX");
--- src/test/java/com/sample/tests/AmazonHomePageTest.java
+++ src/test/java/com/sample/tests/AmazonHomePageTest.java
@@ -1,7 +1,6 @@
 package com.sample.tests;
 
 
-
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
@@ -16,13 +15,13 @@ public class AmazonHomePageTest extends TestBase{
 		AmazonHomePage amazonHomePage= new AmazonHomePage(driver);
 		amazonHomePage.navigateToHomePage();
 		if(amazonHomePage.getLinksOnHomePage()==0){
-			Reporter.log("Links are not Found on the AmazonHomePage!!");
-			Assert.assertTrue(false,"Links are not Found on the AmazonHomePage!!");
+		Reporter.log("Links are not Found on the AmazonHomePage!!");
+			Assert.assertTrue(false);
 		}
 		else
 		{
-			Assert.assertTrue(true,amazonHomePage.getLinksOnHomePage()+" :Links are Found on the AmazonHomePage!");
-			Reporter.log(amazonHomePage.getLinksOnHomePage()+" :Links are Found on the AmazonHomePage!");
+			Assert.assertTrue(true);
+			//Reporter.log(amazonHomePage.getLinksOnHomePage()+" :Links are Found on the AmazonHomePage!");
 		}
 		
 	}
